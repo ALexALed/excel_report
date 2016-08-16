74cd32981b4333e6f99af122a28ba4baee2886aa
alexaled <alexaled@gmail.com>
2016-08-16 16:27:56 +0300

    Fixes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 import xlrd, xlwt
-from xlutils.copy import copy
+
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -12,22 +12,23 @@ class Application(tk.Frame):
         self.target = ''
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Выбор банка"
-        self.hi_there["command"] = self.select_source
-        self.hi_there.pack(side="top")
+        self.choice_source = tk.Button(self)
+        self.choice_source["text"] = "Выбор банка"
+        self.choice_source["command"] = self.select_source
+        self.choice_source.pack(side="top")
 
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Выбор отчета по счетам"
-        self.hi_there["command"] = self.select_target
-        self.hi_there.pack(side="top")
+        self.choice_target = tk.Button(self)
+        self.choice_target["text"] = "Выбор отчета по счетам"
+        self.choice_target["command"] = self.select_target
+        self.choice_target.pack(side="top")
 
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Старт отчета"
-        self.hi_there["command"] = self.run_report
-        self.hi_there.pack(side="top")
+        self.start_report = tk.Button(self)
+        self.start_report["text"] = "Старт отчета"
+        self.start_report["command"] = self.run_report
+        self.start_report.pack(side="top")
 
-
+        self.report_text = tk.Text(self, height=200, width=300)
+        self.report_text.pack(side="bottom")
 
         self.quit = tk.Button(self, text="Выход", fg="red",
                               command=root.destroy)
@@ -72,9 +73,16 @@ class Application(tk.Frame):
                 if r['order'] == v['order'] and v['value'] and r['value'] != v['value']:
                     new_values[k] = v['value']
 
-        for n,v in new_values.items():
-            s.write(n, 8, v)
-        wb.save(self.target.replace('.xls', '1.xls'))
+        not_equals = []
+        for n, v in new_values.items():
+            not_equals.append("Для строки " + n + " новое значение в банке " + v )
+
+        self.report_text.insert(0, "Несовпадения сумм:")
+        self.report_text.insert(1, '\n'.join(not_equals))
+
+
+        # s.write(n, 8, v)
+        # wb.save(self.target.replace('.xls', '1.xls'))
 
 root = tk.Tk()
 app = Application(master=root)
